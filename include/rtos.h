8e97840a2c4da907c799140aaad0d0f44d21bb80
Epeteys <91483378+Epeteys@users.noreply.github.com>
2022-02-28 02:09:11 -0800

    Software comms update
    added formatting to UART_run() for the software stuff and some global variables

--- include/rtos.h
+++ include/rtos.h
@@ -23,6 +23,14 @@ extern xTaskHandle accelerometer_task;
 extern xTaskHandle compass_task;
 
 extern nmea_parser_handle_t nmea_hdl;
+extern int accelx,accely,accelz;
+extern int gyrx,gyry,gyrz;
+extern int lid1,lid2,lid3;
+extern int heading;
+extern int rpm1,rpm2;
+extern int tmp1,tmp2;
+extern int wet1,wet2;
+extern int longitude,latitude;
 
 void RTOSInit(void);
 
--- src/communications.c
+++ src/communications.c
@@ -2,13 +2,33 @@
 
 void UART_run()
 {
-    int output_length = 19;                                             //the number of characters in each output message
-    char out[output_length + 1];                                        //buffer to store output message. Needs extra index at end or sprintf has a fit I think.
+    int output_length = 16;                                             //the number of characters in each output message
+    char out[output_length + 1];                                        //buffer to store output message. Needs extra index at end or sprintf adds an endline character over message.
+    char gps_buffer[23+1];
 
-    float lati;                                                         //example variable storing sensor reading. These would normally be global and if they are non-int/char/float converting them in the sensor's read funciton might be best
-
-    sprintf(out,"gps-lati:%09.5f\n",lati);                              //output message to be sent -> can be copypasted to to make messages for any (int/char/float/etc.) global variable, just not sure how many we'll need yet. Might need edits if float value is >999
+    sprintf(out,"ACC-%3d-%3d-%3d\n",accelx,accely,accelz);                              //output message to be sent -> can be copypasted to to make messages for any (int/char/float/etc.) global variable, just not sure how many we'll need yet. Might need edits if float value is >999
     uart_write_bytes(UART_NUM_0, (const char *) out, output_length);    //outputs buffer to port0, each character is 1 byte as per ASCII, output must have exactly the same number of characters as the output_length variable
+
+    sprintf(out,"GYR-%3d-%3d-%3d\n",gyrx,gyry,gyrz);                              
+    uart_write_bytes(UART_NUM_0, (const char *) out, output_length);
+
+    sprintf(out,"LID-%3d-%3d-%3d\n",lid1,lid2,lid3);                              
+    uart_write_bytes(UART_NUM_0, (const char *) out, output_length);
+
+    sprintf(out,"COM-%3d\n",heading);                              
+    uart_write_bytes(UART_NUM_0, (const char *) out, output_length-8);
+
+    sprintf(out,"RPM-%3d-%3d-%3d\n",rpm1,rpm2);                              
+    uart_write_bytes(UART_NUM_0, (const char *) out, output_length);
+
+    sprintf(out,"TMP-%3d-%3d\n",tmp1,tmp2);                              
+    uart_write_bytes(UART_NUM_0, (const char *) out, output_length);
+
+    sprintf(out,"WET-%3d-%3d\n",wet1,wet2);                              
+    uart_write_bytes(UART_NUM_0, (const char *) out, output_length);
+
+    sprintf(gps_buffer,"GPS-%9d-%9d\n",longitude,latitude);                              
+    uart_write_bytes(UART_NUM_0, (const char *) out, 23);
 }
 
 void I2C_run()
