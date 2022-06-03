5b11913676a31282671e00360ab5b1a3653112bf
Evan <evanpeters@telus.net>
2022-06-02 21:01:49 -0700

    Update config.c

--- src/config.c
+++ src/config.c
@@ -1,6 +1,20 @@
 #include "sensors.h"
 #include "main.h"
 
+void UARTInit()
+{
+const uart_port_t uart_num = UART_NUM_0;
+    uart_config_t uart_config = {                                //uart 0 config
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+    };
+    uart_param_config(uart_num, &uart_config);
+    uart_driver_install(UART_NUM_0, 2048, 0, 0, NULL, 0);
+}
+
 i2c_cmd_handle_t compass_config_run(void)
 {
     ESP_LOGD(tag, ">> hmc5883l");
